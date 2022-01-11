d1ba72475f9ddd5f07d6d6460292a8a6480c3986
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-01-11 10:02:25 +0100

    Client added

---
+++ client.py
@@ -0,0 +1,19 @@
+#! /home/rodrigo/yes/bin/python
+
+import os, sys
+
+os.environ['RAPHTORY_BUILD_SERVERS']="1"
+os.environ['RAPHTORY_PARTITION_SERVERS']="1"
+os.environ['RAPHTORY_BUILDERS_PER_SERVER']="1"
+os.environ['RAPHTORY_PARTITIONS_PER_SERVER']="1"
+
+os.environ['RAPHTORY_BIND_ADDRESS']="localhost"
+os.environ['RAPHTORY_BIND_PORT']="1605"
+
+os.environ['RAPHTORY_LEADER_ADDRESS']="localhost"
+os.environ['RAPHTORY_LEADER_PORT']="1600"
+
+# Change the RAM asigned to Java (Scala)
+os.environ["JAVA_OPTS"]="-XX:+UseShenandoahGC -XX:+UseStringDeduplication -Xms2G -Xmx2G -Xss128M"
+
+os.system('scala -classpath raphtory.jar:' + sys.argv[1] + " es.dit.upm.Client client")
